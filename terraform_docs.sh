4a2974e99b88bd0eae3b4287c9c86a0754cd2eb1
Steven Nemetz <snemetz@wiser.com>
2018-07-25 09:49:42 -0700

    Support no README and README without markers

--- terraform_docs.sh
+++ terraform_docs.sh
@@ -20,17 +20,32 @@ done
 
 readonly tmp_file=$(mktemp)
 readonly text_file="README.md"
+markers_block='
+<!-- BEGINNING OF TERRAFORM-DOCS HOOK -->
+<!-- END OF TERRAFORM-DOCS HOOK -->
+'
 
 for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
   path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
 
   pushd "$path_uniq" > /dev/null
 
-  if [[ ! -f "$text_file" ]]; then
-    popd > /dev/null
-    continue
+  ## Create README.md if it does not exist
+  if [[ ! -f "${text_file}" ]]; then
+    # TODO: improve with a base template
+    touch ${text_file}
+    echo "Creating ${text_file}, please git add."
+    file_changed=1
   fi
 
+  ## Add markers if they don't exist
+  if grep -q -v "BEGINNING OF TERRAFORM-DOCS HOOK" ${text_file}; then
+    echo "${markers_block}" >>${text_file}
+    echo "Updating ${text_file}, please git add."
+    file_changed=1
+  fi
+
+  ## Generate docs and add to README.md
   terraform-docs md ./ > "$tmp_file"
 
   # Replace content between markers with the placeholder - https://stackoverflow.com/questions/1212799/how-do-i-extract-lines-between-two-line-delimiters-in-perl#1212834
@@ -43,3 +58,7 @@ for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
 
   popd > /dev/null
 done
+
+#if [ "${file_changed}" == 1 ]; then
+#  exit 1
+#fi
