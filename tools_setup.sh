1a3b8dff93b14889e6edea81524f4f619f3171e8
Steven Nemetz <snemetz@wiser.com>
2018-07-26 14:25:59 -0700

    Working script. Now needs to be turned into a hook

--- tools_setup.sh
+++ tools_setup.sh
@@ -1,23 +1,23 @@
 
 # Install/upgrade all tools scripts depend on
 
-# terraform, tflint, terraform-docs, dot?,
+tool_list='terraform terraform-docs graphviz jq tflint'
 
-# if mac, brew (what can {create rules for what doesn't exist?})
-brew install terraform terraform-docs graphviz
-brew tap wata727/tflint
-brew install tflint
+if [ $(uname -s) == "Darwin" ]; then
+  brew_installed=$(brew list)
+  outdated_apps=$(brew outdated --json=v1 | jq -r .[].name)
+  for app in ${tool_list}; do
+    if [ $(echo ${brew_installed} | grep ${app} | wc -l )  -gt 0 ]; then
+      if [ $(echo ${outdated_apps} | grep ${app} | wc -l )  -gt 0 ]; then
+        brew upgrade ${app}
+      fi
+    else
+      if [ "${app}" == "tflint" ]; then
+        brew tap wata727/tflint
+      fi
+      brew install ${app}
+    fi
+  done
+fi
+
 #terraform_landscape tfenv
-
-apk update
-apk add jq wget
-# Get latest version of tflint (v0.7.0 test if still need to exclude modules. Any other changes)
-pkg_arch=linux_amd64
-dl_url=$(curl -s https://api.github.com/repos/wata727/tflint/releases/latest | jq -r ".assets[] | select(.name | test(\"${pkg_arch}\")) | .browser_download_url")
-wget ${dl_url}
-unzip tflint_linux_amd64.zip
-mkdir -p /usr/local/tflint/bin
-# Setup PATH for later run steps - ONLY for Bash and not in Bash
-#echo 'export PATH=/usr/local/tflint/bin:$PATH' >> $BASH_ENV
-echo "Installing tflint..."
-install tflint /usr/local/tflint/bin
