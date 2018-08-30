5154879ea2eb78bff6dbe29c6d4401fb34a405c3
Steven Nemetz <snemetz@wiser.com>
2018-08-30 13:53:23 -0700

    Change from sed to perl

--- tflint.sh
+++ tflint.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 
 # TODO: build config to ignore modules until supports registry paths
-if [ $(uname -s) == "Darwin" ]; then
-  cmd_sed='sed -E'
-else
-  cmd_sed='sed -r'
-fi
 #echo "Configuring tflint..."
 tf_ver=$(terraform version | awk 'FNR <= 1' | cut -dv -f2)
 echo -e "\tConfig tflint for terraform version: ${tf_ver}"
 if [ -f '.tflint.hcl' ]; then
-  sed -i "/terraform_version =/s/\".*\"/\"${tf_ver}\"/" .tflint.hcl
+  perl -pi -e "s/\".*\"/\"${tf_ver}\"/ if /terraform_version =/" .tflint.hcl
 else
   {
   echo -e "config {\nterraform_version = \"${tf_ver}\"\ndeep_check = true\nignore_module = {"
-  for module in $(grep -h '[^a-zA-Z]source[ =]' *.tf | ${sed_cmd} 's/.*=\s+//' | sort -u); do
+  for module in $(grep -h '[^a-zA-Z]source[ =]' *.tf | perl -pe 's/^.*=\s+//' | sort -u); do
     # if not ^"../
     echo "${module} = true"
   done
