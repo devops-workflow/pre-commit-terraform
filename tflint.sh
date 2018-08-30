0d96ea69dc1d5a434530079ae0da6fa9d2e6f374
Steven Nemetz <snemetz@wiser.com>
2018-08-30 13:05:39 -0700

    Fix for Mac

--- tflint.sh
+++ tflint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 # TODO: build config to ignore modules until supports registry paths
+if [ $(uname -s) == "Darwin" ]; then
+  cmd_sed='sed -E'
+else
+  cmd_sed='sed -r'
+fi
 #echo "Configuring tflint..."
 tf_ver=$(terraform version | awk 'FNR <= 1' | cut -dv -f2)
 echo -e "\tConfig tflint for terraform version: ${tf_ver}"
@@ -9,7 +14,7 @@ if [ -f '.tflint.hcl' ]; then
 else
   {
   echo -e "config {\nterraform_version = \"${tf_ver}\"\ndeep_check = true\nignore_module = {"
-  for module in $(grep -h '[^a-zA-Z]source[ =]' *.tf | sed -r 's/.*=\s+//' | sort -u); do
+  for module in $(grep -h '[^a-zA-Z]source[ =]' *.tf | ${sed_cmd} 's/.*=\s+//' | sort -u); do
     # if not ^"../
     echo "${module} = true"
   done
